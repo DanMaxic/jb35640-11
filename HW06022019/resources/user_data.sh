c6395285beed72f3978e837f535715090a436c96
danmaxic <DanMaxic@users.noreply.github.com>
2019-02-06 20:38:36 +0200

    Update user_data.sh

--- HW06022019/resources/user_data.sh
+++ HW06022019/resources/user_data.sh
@@ -113,8 +113,8 @@ function configureLogins(){
     groupadd remote_users
     echo "remote_users    ALL=(ALL)    NOPASSWD:ALL" >> /etc/sudoers
     echo "$${SSH_USERNAME}    ALL=(ALL)    NOPASSWD:ALL" >> /etc/sudoers
-    useradd ${SSH_USERNAME} -G remote_users
-    echo "$${SSH_PSSWORD}" | sudo passwd ${SSH_USERNAME} --stdin
+    useradd $${SSH_USERNAME} -G remote_users
+    echo "$${SSH_PSSWORD}" | sudo passwd $${SSH_USERNAME} --stdin
 
 }
 
